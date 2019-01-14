bceecb1de3f00ab94758ce7fb5b45c0c40a85805
css521 <1156986964@qq.com>
2019-01-14 18:29:29 +0800

    Find All Duplicates in an Array

---
+++ leetcode/FindDuplicates.cpp
@@ -0,0 +1,30 @@
+
+/*
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+*/
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums)
+    {
+        vector<int>vec;
+        map<int,int>hash;
+        for(int i=0;i<nums.size();++i)
+            hash[nums[i]]++;
+        for(auto it=hash.begin();it!=hash.end();++it)
+            if(it->second==2)
+              vec.push_back(it->first);
+        return vec;
+    }
+};
