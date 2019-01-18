6cfdc4f4a2d0e9b8c1dce8c802a9a0ec5b366e8c
css521 <1156986964@qq.com>
2019-01-18 20:45:19 +0800

    Increasing Triplet Subsequence

---
+++ leetcode/IncreasingTriplet.cpp
@@ -0,0 +1,41 @@
+/*
+Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+
+Formally the function should:
+
+Return true if there exists i, j, k 
+such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+Note: Your algorithm should run in O(n) time complexity and O(1) space complexity.
+
+Example 1:
+
+Input: [1,2,3,4,5]
+Output: true
+Example 2:
+
+Input: [5,4,3,2,1]
+Output: false
+*/
+
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) 
+    {
+        int min1=INT_MAX;
+        int min2=INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<=min1)
+                min1=nums[i];
+            else
+            {
+                if(nums[i]<=min2)
+                    min2=nums[i];
+                else
+                    return true;
+            }
+        }
+        return false;  
+    }
+};
