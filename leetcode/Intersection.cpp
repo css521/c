1299a77c5a63f17cb82df83560166d4ee9e1d4eb
css521 <1156986964@qq.com>
2019-01-25 12:57:43 +0800

    Intersection of Two Arrays

---
+++ leetcode/Intersection.cpp
@@ -0,0 +1,32 @@
+
+/*
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+*/
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        unordered_map<int,int>hash;
+        vector<int>res;
+        for(auto it:nums1)hash[it]++;
+        for(auto it:nums2)
+        {
+            if(hash[it])
+            {
+                res.push_back(it);
+                hash[it]=0;
+            }
+        }
+        return res;
+    }
+};
