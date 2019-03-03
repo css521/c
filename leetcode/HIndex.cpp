709411d83c8af9c6862503b6c0edb106fa34c415
css521 <1156986964@qq.com>
2019-03-03 20:20:48 +0800

    H-Index

---
+++ leetcode/HIndex.cpp
@@ -0,0 +1,32 @@
+
+/*
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+Example:
+
+Input: citations = [3,0,6,1,5]
+Output: 3 
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had 
+             received 3, 0, 6, 1, 5 citations respectively. 
+             Since the researcher has 3 papers with at least 3 citations each and the remaining 
+             two with no more than 3 citations each, her h-index is 3.
+
+*/
+class Solution {
+public:
+    int hIndex(vector<int>& citations) 
+    {
+        if(citations.empty())return 0;
+        sort(citations.rbegin(),citations.rend());
+        priority_queue<int> q;
+        for(int i=0;i<citations.size();i++)
+        {
+            if(i+1<=citations[i])
+                q.emplace(i+1);
+        }
+        return q.empty()?0:q.top();
+        
+    }
+};
