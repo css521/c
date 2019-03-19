c5fec7aa4c47c44c20f9e9a57573690bfea6dfa2
css521 <1156986964@qq.com>
2019-03-19 16:37:41 +0800

    Clumsy Factorial

---
+++ leetcode/Clumsy.cpp
@@ -0,0 +1,42 @@
+/*
+Normally, the factorial of a positive integer n is the product of all positive integers less than or equal to n.  For example, factorial(10) = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1.
+
+We instead make a clumsy factorial: using the integers in decreasing order, we swap out the multiply operations for a fixed rotation of operations: multiply (*), divide (/), add (+) and subtract (-) in this order.
+
+For example, clumsy(10) = 10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1.  However, these operations are still applied using the usual order of operations of arithmetic: we do all multiplication and division steps before any addition or subtraction steps, and multiplication and division steps are processed left to right.
+
+Additionally, the division that we use is floor division such that 10 * 9 / 8 equals 11.  This guarantees the result is an integer.
+
+Implement the clumsy function as defined above: given an integer N, it returns the clumsy factorial of N.
+
+ 
+
+Example 1:
+
+Input: 4
+Output: 7
+Explanation: 7 = 4 * 3 / 2 + 1
+Example 2:
+
+Input: 10
+Output: 12
+Explanation: 12 = 10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1
+*/
+
+class Solution {
+public:
+    int clumsy(int N) {
+        int op=0;
+        int temp=N;
+        int ans=0;
+        for(int i=N-1;i>=1;i--)
+        {
+            if(op==0)temp*=i;
+            if(op==1)temp/=i;
+            if(op==2){ans+=temp+i;temp=0;}
+            if(op==3)temp=-i;
+            op=(op+1)%4;
+        }
+        return temp+ans;
+    }
+};
